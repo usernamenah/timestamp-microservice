d45d98959751d93358a5e0d9553272a93a8be928
P.Sujith <sujithpappala777@gmail.com>
2025-05-29 12:44:41 +0530

    url-shortener-microservice

--- server.js
+++ server.js
@@ -1,29 +1,54 @@
 const express = require('express');
+const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
 const app = express();
 
-// Enable CORS (optional, for testing on freeCodeCamp)
-const cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));
+// Basic in-memory store
+const urls = {};
+let counter = 1;
 
-// Root route
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// Homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// API endpoint for /api/whoami
-app.get('/api/whoami', (req, res) => {
-  const ipaddress = req.ip;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
+// POST endpoint to shorten a URL
+app.post('/api/shorturl', (req, res) => {
+  const original_url = req.body.url;
 
-  res.json({
-    ipaddress,
-    language,
-    software
+  // Remove protocol (http:// or https://) for DNS check
+  const urlWithoutProtocol = original_url.replace(/^https?:\/\//, '');
+
+  dns.lookup(urlWithoutProtocol, (err, address) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    } else {
+      const short_url = counter++;
+      urls[short_url] = original_url;
+      res.json({ original_url, short_url });
+    }
   });
 });
 
-// Listen on port 3000 or provided environment port
+// Redirect endpoint
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const short_url = req.params.short_url;
+  const original_url = urls[short_url];
+
+  if (original_url) {
+    res.redirect(original_url);
+  } else {
+    res.json({ error: 'No short URL found for given input' });
+  }
+});
+
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
